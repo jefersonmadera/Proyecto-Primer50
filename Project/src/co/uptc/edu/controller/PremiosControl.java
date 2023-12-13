0e5dbf5253d6cc4d2796e12fbb07eca2491991bc
Vargasx-x <vargasgonzalezjuanesteban01@gmail.com>
2023-12-12 20:51:16 -0500

    Avances

--- Project/src/co/uptc/edu/controller/PremiosControl.java
+++ Project/src/co/uptc/edu/controller/PremiosControl.java
@@ -3,7 +3,6 @@ package co.uptc.edu.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import co.uptc.edu.model.Premio;
 import co.uptc.edu.model.DetallesPremio;
 
 public class PremiosControl {
